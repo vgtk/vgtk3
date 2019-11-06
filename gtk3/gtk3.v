8914b3c13ca5b8ed784598c8c98e350c9b009572
zenith391 <pawalamde@gmail.com>
2019-11-06 21:12:58 +0100

    Text Fields (Entries) and HBoxes

--- gtk3/gtk3.v
+++ gtk3/gtk3.v
@@ -14,7 +14,12 @@ pub struct Alignment {
 	gtk_widget &C.GtkWidget
 }
 
+// Struct definition
 pub struct VBox {
+	gtk_widget &C.GtkWidget
+}
+
+pub struct HBox {
 	gtk_widget &C.GtkWidget
 }
 
@@ -26,6 +31,11 @@ pub struct Button {
 	gtk_widget &C.GtkWidget
 }
 
+pub struct Entry {
+	gtk_widget &C.GtkWidget
+}
+
+// Actual code
 fn init() {
 	C.gtk_init(0, [""].data) // TODO: use os library for arguments
 }
@@ -44,6 +54,13 @@ pub fn new_button(label string) Button {
 	return btn
 }
 
+pub fn new_entry() Entry {
+	btn := &Entry{
+		gtk_widget: C.gtk_entry_new()
+	}
+	return btn
+}
+
 pub fn new_alignment(xalign f32, yalign f32, xscale f32, yscale f32) Alignment {
 	align := &Alignment{
 		gtk_widget: C.gtk_alignment_new(xalign, yalign, xscale, yscale)
@@ -56,6 +73,13 @@ pub fn new_vbox(homogeneous bool, spacing int) VBox {
 		gtk_widget: C.gtk_vbox_new(homogeneous, spacing)
 	}
 	return vbox
+}
+
+pub fn new_hbox(homogeneous bool, spacing int) HBox {
+	hbox := &HBox{
+		gtk_widget: C.gtk_hbox_new(homogeneous, spacing)
+	}
+	return hbox
 }
 
 // This function is blocking!
@@ -136,4 +160,36 @@ pub fn (v &VBox) get_gtk_widget() &C.GtkWidget {
 
 pub fn (v VBox) add(widget Widgeter) {
 	C.gtk_container_add(v.gtk_widget, widget.get_gtk_widget())
+}
+
+// HBox struct
+
+pub fn (h &HBox) get_gtk_widget() &C.GtkWidget {
+	return h.gtk_widget
+}
+
+pub fn (h HBox) add(widget Widgeter) {
+	C.gtk_container_add(h.gtk_widget, widget.get_gtk_widget())
+}
+
+// Entry struct
+
+pub fn (e &Entry) get_gtk_widget() &C.GtkWidget {
+	return e.gtk_widget
+}
+
+pub fn (e Entry) get_text() string {
+	return tos3(C.gtk_entry_get_text(e.gtk_widget))
+}
+
+pub fn (e Entry) set_text(text string) {
+	C.gtk_entry_set_text(e.gtk_widget, text.str)
+}
+
+pub fn (e Entry) set_visibility(visible bool) {
+	C.gtk_entry_set_visibility(e.gtk_widget, visible)
+}
+
+pub fn (e Entry) set_invisible_char(ch byte) {
+	C.gtk_entry_set_invisible_char(e.gtk_widget, ch)
 }
--- test.v
+++ test.v
@@ -9,6 +9,10 @@ fn btn_clicked(w &C.GtkWidget, btn &gtk3.Button) {
 	}
 }
 
+fn alert_clicked(w &C.GtkWidget, btn &gtk3.Button) {
+	btn.set_label("All Fine!")
+}
+
 fn win_destroy(w &C.GtkWidget, win gtk3.Window) {
 	gtk3.quit() // necessary as GTK3 won't exit itself when window is destroyed.
 }
@@ -19,12 +23,24 @@ fn main() {
 	align := gtk3.new_alignment(0.5, 0.5, 0, 0)
 	btn := gtk3.new_button("GTK3-V is ..")
 	btn2 := gtk3.new_button("Download Alpha now!")
+	alert := gtk3.new_button("Alert!")
+
+	hbox := gtk3.new_hbox(false, 20)
+	entry := gtk3.new_entry()
 
 	btn.set_size(200, 100)
 	btn2.set_size(100, 50)
+	alert.set_size(80, 20)
+	entry.set_text("Good Night!")
 	btn.add_on_clicked(btn_clicked)
+	alert.add_on_clicked(alert_clicked)
+
+	hbox.add(entry)
+	hbox.add(alert)
+
 	vbox.add(btn)
-	vbox.add(btn2)
+	//vbox.add(btn2)
+	vbox.add(hbox)
 	align.add(vbox)
 
 	window.set_size(640, 480)
