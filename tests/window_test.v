ebc6b5ec97874ac05f5bbdbb8d49ce5de2fba8e5
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2019-12-19 19:23:00 +0800

    add test for window size

--- tests/window_test.v
+++ tests/window_test.v
@@ -10,6 +10,18 @@ fn test_window_title() {
 	gtk.main_quit()
 }
 
+fn test_window_size() {
+	window := gtk.new_window()
+	btn := gtk.new_button('test')
+	window.add(btn)
+	window.set_default_size(500, 250)
+	w, h := window.get_default_size()
+	assert w == 500
+	assert h == 250
+	window.show()
+	gtk.main_quit()
+}
+
 
 // fn main_and_quit() {
 // 	main_and_quit()
