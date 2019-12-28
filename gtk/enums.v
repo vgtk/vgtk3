2bf890fcebec0f132b4229d02887d427561eb997
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2019-12-28 21:19:44 +0800

    add more enums

--- gtk/enums.v
+++ gtk/enums.v
@@ -28,3 +28,39 @@ pub enum TextDirection {
 	ltr
 	rtl
 }
+
+pub enum StateFlags {
+	normal
+	active
+	prelight
+	selected
+	insensitive
+	inconsistent
+	focused
+	backdrop
+	dir_ltr
+	dir_rtl
+	link
+	visited
+	checked
+	drop_active
+}
+
+pub enum SizeRequestMode {
+	height_for_width
+	width_for_height
+	constant_size
+}
+
+pub enum Align {
+	fill
+	start
+	end
+	center
+	baseline
+}
+
+pub enum Orientation {
+	horizontal
+	vertical
+}
