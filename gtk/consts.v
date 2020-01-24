ed3633d0e31e0c3d878943046df97ead57e82d2f
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-01-24 12:03:21 +0800

    gtk: Add version constants

--- gtk/consts.v
+++ gtk/consts.v
@@ -7,3 +7,10 @@ pub const (
 	PACK_START = PackType(C.GTK_PACK_START)
 	PACK_END   = PackType(C.GTK_PACK_END)
 )
+
+pub const (
+	MAJOR_VERSION = C.gtk_major_version
+	MINOR_VERSION = C.gtk_minor_version
+	MICRO_VERSION = C.gtk_micro_version
+	VERSION = MAJOR_VERSION.str() + '.' + MINOR_VERSION.str() + '.' + MICRO_VERSION.str()
+)
