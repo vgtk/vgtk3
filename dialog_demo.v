dc63c27a5d8b8f197ff259fda888bd2ecd740626
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-01-29 16:34:37 +0800

    dialog_demo: Remove unecessary code

--- dialog_demo.v
+++ dialog_demo.v
@@ -1,12 +1,8 @@
 module main
 
 import gtk
-import gdk
 
 fn main() {
-	// a := gtk.VERSION
-	// println(a)
-	// a := C.GTK_RESPONSE_NONE
 	window := gtk.new_window()
 	window.set_title('Main window')
 	window.set_default_size(500, 250)
@@ -25,27 +21,17 @@ fn main() {
 	gtk.main()
 }
 
-[inline]
-fn num(num int) int {
-	mut c := 0
-	if num > 0 {
-		c++
-	}
-	return c
-}
-
 fn on_btn_show_dialog_clicked(button gtk.Button, data voidptr) {
 	dialog := &gtk.Dialog(data)
 
 	label := gtk.new_label('Are you sure want to quit?')
 	box := dialog.get_content_area()
-
-	println(num(-1))
+	box.add(label)
 
 	label.show()
 	res := dialog.run()
 
-	if res == gtk.ResponseType.yes {
+	if res == .yes {
 		println('Yes')
 		gtk.main_quit()
 	} else {
