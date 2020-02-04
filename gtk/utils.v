86738664d4b936cf7bddb68be6826ca696511e8a
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-02-04 19:06:29 +0800

    gtk/utils: Add c []int to v []int

--- gtk/utils.v
+++ gtk/utils.v
@@ -13,3 +13,17 @@ fn carray_string_to_array_string(arr &charptr) []string {
     unsafe { free(arr) }
     return arr_str
 }
+
+fn carray_int_to_array_int(arr &intptr) []int {
+    mut arr_int := []int
+    if isnil(arr) { return arr_int }
+    mut i := 0
+    for {
+        elem := int(arr[i])
+        if elem == 0 { break }
+        arr_int << elem
+        i++
+    }
+    unsafe { free(arr) }
+    return arr_int
+}
