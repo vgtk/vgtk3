3e37d29f98492657a268e675221a8ff61fa9e1a0
pancake <pancake@nowsecure.com>
2020-11-08 19:14:51 +0100

    gtk: add gtk.new_separator_menu_item() and use it in demo.v (#21)
    Co-authored-by: pancake <pancake@nopcode.org>

--- demo.v
+++ demo.v
@@ -3,10 +3,10 @@ module main
 import gtk
 
 fn btn_clicked(btn gtk.Button, data voidptr) {
-	if btn.get_label() == "VGTK3 is .." {
-		btn.set_label("VGTK3 is awesome!")
+	if btn.get_label() == 'VGTK3 is ..' {
+		btn.set_label('VGTK3 is awesome!')
 	} else {
-		btn.set_label("VGTK3 is ..")
+		btn.set_label('VGTK3 is ..')
 	}
 }
 
@@ -15,7 +15,7 @@ fn menu_exit(mi gtk.MenuItem, data voidptr) {
 }
 
 fn alert_clicked(btn gtk.Button, data voidptr) {
-	btn.set_label("All Fine!")
+	btn.set_label('All Fine!')
 }
 
 fn win_destroy(win gtk.Window, data voidptr) {
@@ -27,15 +27,21 @@ fn main() {
 	window := gtk.new_window()
 	vbox := gtk.new_vbox(20)
 	vbox.set_halign(.fill)
-	btn := gtk.new_button_with_label("VGTK3 is ..")
-	btn2 := gtk.new_button_with_label("I'm useless!")
-	alert := gtk.new_button_with_label("Alert!")
+	btn := gtk.new_button_with_label('VGTK3 is ..')
+	btn2 := gtk.new_button_with_label('Im useless!')
+	alert := gtk.new_button_with_label('Alert!')
 
 	bar := gtk.new_menu_bar()
+
 	menu := gtk.new_menu()
-	file_me := gtk.new_menu_item_with_label("File")
-	quit := gtk.new_menu_item_with_label("Quit")
+	file_me := gtk.new_menu_item_with_label('File')
 	file_me.set_submenu(menu)
+
+	about := gtk.new_menu_item_with_label('About')
+	quit := gtk.new_menu_item_with_label('Quit')
+	menu.append(gtk.new_menu_item_with_label('File'))
+	menu.append(about)
+	menu.append(gtk.new_separator_menu_item())
 	menu.append(quit)
 	bar.append(file_me)
 
@@ -46,15 +52,15 @@ fn main() {
 	btn.set_size(200, 100)
 	btn2.set_size(100, 50)
 	alert.set_size(80, 20)
-	entry.set_text("Good Night!")
-	btn.on("clicked", btn_clicked, &window)
+	entry.set_text('Good Night!')
+	btn.on('clicked', btn_clicked, &window)
 
-	gtk.accel_map_add_entry("GTK-Test/File/Quit", 65, 0)
-	quit.set_accel_path("GTKTest/File/Quit")
+	gtk.accel_map_add_entry('GTK-Test/File/Quit', 65, 0)
+	quit.set_accel_path('GTKTest/File/Quit')
 	quit.set_use_underline(true)
-	quit.on("activate", menu_exit, &window)
+	quit.on('activate', menu_exit, &window)
 
-	alert.on("clicked", alert_clicked, &window)
+	alert.on('clicked', alert_clicked, &window)
 
 	hbox.add(entry)
 	hbox.add(alert)
@@ -65,9 +71,9 @@ fn main() {
 	vbox.add(hbox)
 
 	window.set_position(.center)
-	window.set_title("I'm made with V")
+	window.set_title('Im made with V')
 	window.add(vbox)
-	window.on("destroy", win_destroy, voidptr(0))
+	window.on('destroy', win_destroy, voidptr(0))
 	window.show_all()
 	gtk.main()
 }
